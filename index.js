d6429210e8ea13f39d7e617905966db3035ecd26
Bijoy Thomas <bijoyjth@gmail.com>
2018-02-18 19:52:08 -0600

    Add sanctuary lib

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ R = require('ramda'),
 _ = require('lodash'),
 moment = require('moment'),
 RF = require('ramda-fantasy')
+const {create, env}  = require('sanctuary')
+const S = create({checkTypes: process.env.NODE_ENV !== 'production', env})
 
 exports.handler = (event, context, callback) => {
 
   try {
     let code = event['body-json'].item.message.message.split('/ramda')[1]
-    let result = vm.runInNewContext(code, R.mergeAll([R, RF, {R, moment, _}]), {timeout: 5000})
+    let result = vm.runInNewContext(code, R.mergeAll([R, RF, {R, moment, _, S}]), {timeout: 5000})
     callback(null, {
         'color': 'green',
         'message': util.inspect(result),
@@ -26,7 +28,7 @@ exports.handler = (event, context, callback) => {
     console.error(err)
     callback(null, {
         'color': 'red',
-        'message': 'Check your functional fu my friend',
+        'message': 'Check your functional fu my friend' + '\n' + err,
         'notify': false,
         'message_format': 'text'
     })
