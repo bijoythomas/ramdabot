94cdfc6859515b442918fc826b7c6f2e39f1e4dc
Bijoy Thomas <bijoyjth@gmail.com>
2018-08-21 18:04:24 -0500

    add ramda function description enhancement

--- index.js
+++ index.js
@@ -20,17 +20,45 @@ exports.handler = (event, context, callback) => {
       callback(null, 'Run me in the #jsbot channel')
       return
     }
-    let code = event.text
+    let code = event.text.trim()
+
+    // checking if requesting a ramda function definition
+    if (R.test(/^[a-zA-Z]+$/, code) && R[code]) {
+      let contents = fs.readFileSync('./node_modules/ramda/src/' + code + '.js', 'utf8')
+      let comments = R.compose(
+        R.join('\n'),
+        R.head,
+        R.splitWhen(line => line.startsWith(' */')),
+        R.last,
+        R.splitWhen(line => line.startsWith('/**')),
+        R.split('\n')
+      )(contents)
+      return callback(null, {
+        "response_type": "in_channel",
+        "text": comments,
+        "attachments": [{
+          "color": "#00FF00"
+        }]
+      })
+    }
+
+    // run code
     let result = vm.runInNewContext(code, R.mergeAll([R, RF, {R, moment, _, S}]), {timeout: 5000})
     callback(null, {
       "response_type": "in_channel",
-      "text": util.inspect(result)
+      "text": util.inspect(result),
+      "attachments": [{
+        "color": "#00FF00"
+      }]
     })
   } catch (err) {
     console.error(err)
     callback(null, {
       "response_type": "in_channel",
-      "text": "Check your JS foo my friend"
+      "text": "Check your JS foo my friend",
+      "attachments": [{
+        "color": "#FF0000"
+      }]
     })
   }
 };
